ed827e5073cae95e3dc2e73a75a8fda3ebfa1501
medivh <bochkarev.s.e@yandex.ru>
2023-11-22 20:09:27 +0300

    fixture added

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.fixture
+def one_two_three():
+    return [1, 2, 3]
--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,13 +1,13 @@
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
+def test_get(one_two_three):
+    assert arrs.get(one_two_three, 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+def test_slice(one_two_three):
+    assert arrs.my_slice(one_two_three, 1, 3) == [2, 3]
+    assert arrs.my_slice(one_two_three, 1) == [2, 3]
     assert arrs.my_slice([], 1) == []
-    assert arrs.my_slice([1, 2, 3], None, 3) == [1, 2, 3]
+    assert arrs.my_slice(one_two_three, None, 3) == [1, 2, 3]
